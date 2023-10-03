4c09f5d775197b40fa6c005e110c0a35114584f0
Ibuki <c0a2205559@edu.teu.ac.jp>
2023-10-03 15:26:24 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,73 +4,80 @@ import random
 
 WIDTH, HEIGHT = 1600, 900
 
-#check
+delta = {  # 練習３：移動量辞書
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
+
+def check_bound(obj_rct: pg.Rect):
+    """
+    引数：こうかとんRectかばくだんRect
+    戻り値：タプル（横方向判定結果，縦方向判定結果）
+    画面内ならTrue，画面外ならFalse
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: # 横方向判定
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom: # 縦方向判定
+        tate = False
+    return yoko, tate
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900, 400)  # 練習３：こうかとんの初期座標を設定する
+    """ばくだん"""
+    bd_img = pg.Surface((20, 20))  # 練習１：爆弾Surfaceを作成する
+    bd_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()  # 練習１：SurfaceからRectを抽出する
+    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bd_rct.center = (x, y)  # 練習１：Rectにランダムな座標を設定する
+    vx, vy = +5, +5  # 練習２：爆弾の速度
+
     clock = pg.time.Clock()
-
-    crcl = pg.Surface((20,20),pg.SRCALPHA)
-    pg.draw.circle(crcl,(255, 0, 0),(10, 10), 10)
-    crcl.set_colorkey((0, 0, 0))
-
-    clock = pg.time.Clock()
-
     tmr = 0
-
-    crcl_rect = crcl.get_rect(center = (random.randint(0, WIDTH), random.randint(0, HEIGHT)))
-    kk_rect = kk_img.get_rect(center = (900, 400))
-
-    vx = 5
-    vy = 5
-
-    def is_inside(rect):
-        return screen.get_rect().contains(rect) 
-    
-
-    movement_dict = {
-        pg.K_UP:(0, -5),
-        pg.K_DOWN:(0, 5),
-        pg.K_RIGHT:(5, 0),
-        pg.K_LEFT:(-5,)
-    }
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
             
-        key_lst = pg.key.get_pressed()
-
-        total_movement = [0, 0]
-
-        for key, movement in movement_dict.items():
-            if key_lst[key]:
-                total_movement[0] += movement[0]
-                total_movement[1] += movement[1]
-
-        kk_rect.move_ip(total_movement[0], total_movement[1])
-        crcl_rect.move_ip(vx, vy)
-
+        if kk_rct.colliderect(bd_rct):
+            return
+            
         screen.blit(bg_img, [0, 0])
 
-        if not is_inside(crcl):
-            kk_rect.clamp_ip(screen.get_rect())
-
-        screen.blit(kk_img, kk_rect)
-
-        if not is_inside(crcl_rect):
-            return
+        """こうかとん"""
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for key, mv in delta.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]  # 練習３：横方向の合計移動量
+                sum_mv[1] += mv[1]  # 練習３：縦方向の合計移動量
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])  # 練習３：移動させる
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kk_img, kk_rct)  # 練習３：移動後の座標に表示させる
+        """"ばくだん"""
+        bd_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させ
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:
+            vx *= -1
         
-        screen.blit(crcl, crcl_rect)
-
-
+        if not tate:
+            vy *= -1
+        screen.blit(bd_img, bd_rct)  # 練習１：Rectを使って試しにblit
+        
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 
